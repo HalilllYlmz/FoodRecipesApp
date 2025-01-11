345cc3dad392f1713f59990bb1aeda7641f635fe
halilyilmaz <halilylm1903@gmail.com>
2025-01-12 00:39:13 +0300

    InstructionsFragment completed

--- app/src/main/java/com/example/modernfoodrecipesapp/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/example/modernfoodrecipesapp/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,16 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.example.modernfoodrecipesapp.R
+import com.example.modernfoodrecipesapp.databinding.FragmentIngredientsBinding
+import com.example.modernfoodrecipesapp.databinding.FragmentInstructionsBinding
+import com.example.modernfoodrecipesapp.model.Result
+import com.example.modernfoodrecipesapp.utilities.Constants
 
 class InstructionsFragment : Fragment() {
+
+    private var _binding: FragmentInstructionsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+    ): View {
+        _binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPES_RESULT_KEY)
+
+        binding.instructionsWebView.webViewClient = object : WebViewClient() {}
+        val websiteUrl: String = myBundle!!.sourceUrl
+        binding.instructionsWebView.loadUrl(websiteUrl)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
