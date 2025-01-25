6b1092c46c212171928f0bc6fee031b041769bd5
halilyilmaz <halilylm1903@gmail.com>
2025-01-25 14:17:21 +0300

    Fragment labels changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,8 +77,8 @@ dependencies {
     ksp(libs.androidx.room.compiler)
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.core)
 
     // Retrofit
     implementation(libs.retrofit)
--- app/src/main/java/com/example/modernfoodrecipesapp/model/FoodJoke.kt
+++ app/src/main/java/com/example/modernfoodrecipesapp/model/FoodJoke.kt
@@ -1,8 +1,10 @@
 package com.example.modernfoodrecipesapp.model
 
 import com.google.gson.annotations.SerializedName
+import androidx.room.ColumnInfo
 
 data class FoodJoke(
     @SerializedName("text")
+    @ColumnInfo(name = "text")
     val text: String
 )
